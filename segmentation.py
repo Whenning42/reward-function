dffa6b8d1a5fc0ace2ad65300eb5aa9577d8801d
William Henning <wchenningwchenning@gmail.com>
2019-11-03 12:08:07 -0500

    Got segmentation class working

--- segmentation.py
+++ segmentation.py
@@ -2,12 +2,19 @@
 # with a UID.
 
 from collections import deque
+import torch
 
-# This class uses (N, C, H, W) image layout
+def InBounds(image, pos):
+    return pos[0] >= 0 and \
+           pos[0] < image.shape[0] and \
+           pos[1] >= 0 and \
+           pos[1] < image.shape[1]
+
+
+# This class uses torch batches of images with a (N, C, H, W) batch image layout.
 class SkyrogueSegmenter:
-    def __init__(self, cache = None):
-        if cache is not None:
-            self.cache = cache
+    def __init__(self):
+        self.kUnlabeledComponent = -1
 
     def Threshold(self, images):
         # Input images should be in RGB 0-1 format.
@@ -17,12 +24,64 @@ class SkyrogueSegmenter:
 
         return (images[:, 0:1, :, :] == 1).double()
 
+    @staticmethod
+    def _ConnectComponent(working_image, \
+                          start_position, \
+                          component_label, \
+                          component_bitmap, \
+                          unlabeled_sentinel):
+        to_connect = deque()
+        to_connect.append(start_position)
+        while len(to_connect) > 0:
+            position = to_connect.popleft()
+            for d0 in range(-1, 2):
+                for d1 in range(-1, 2):
+                    next_pos = (position[0] + d0, position[1] + d1)
+                    if not InBounds(working_image, next_pos):
+                        continue
+                    if working_image[next_pos] == unlabeled_sentinel:
+                        working_image[next_pos] = component_label
+                        component_bitmap[next_pos] = 1
+                        to_connect.append(next_pos)
+
+    @staticmethod
+    def _CropToExtent(bitmap):
+        nonzero_indices = bitmap.nonzero()
+        extent = nonzero_indices[:, 0].min(), \
+                 nonzero_indices[:, 0].max(), \
+                 nonzero_indices[:, 1].min(), \
+                 nonzero_indices[:, 1].max()
+        return bitmap[extent[0] : extent[1] + 1, \
+                      extent[2] : extent[3] + 1]
+
+    # Returns a list of lists of pytorch tensors cropped to the size of their
+    # segmentations
     def Segment(self, images):
         assert images.size(1) == 1
         assert images.max() <= 1
         assert images.min() >= 0
 
-        queue = deque()
-        y_pos = images.size(2) // 2
-        for x in range(0, images.size(3)):
-            queue.append()
+        segmentations = []
+        for image in images:
+            components = []
+
+            # Here image is (C, H, W) with C = 1 so we strip the channel here
+            # to get working image into a (H, W) format.
+            working_image = image[0] * self.kUnlabeledComponent
+
+            y = images.size(2) // 2
+            current_component = 1
+            for x in range(0, images.size(3)):
+                if working_image[y, x] == self.kUnlabeledComponent:
+                    component_bitmap = torch.zeros(working_image.size())
+                    self._ConnectComponent(working_image, \
+                                           (y, x), \
+                                           current_component, \
+                                           component_bitmap, \
+                                           self.kUnlabeledComponent)
+
+                    components.append(self._CropToExtent(component_bitmap))
+                    current_component += 1
+
+            segmentations.append(components)
+        return segmentations
--- segmentation_test.py
+++ segmentation_test.py
@@ -1,4 +1,3 @@
-# Using set colorcolumn=65 because it seems nice.
 import pytest
 import torch
 from segmentation import SkyrogueSegmenter
@@ -20,7 +19,7 @@ def test_Threshold():
     # Blue channel of second image
     images[1, 1, 0, 0] = 1
 
-    segmenter = SkyrogueSegmenter(cache = False)
+    segmenter = SkyrogueSegmenter()
     images = segmenter.Threshold(images)
 
     # Threshold should return greyscale images
@@ -41,18 +40,30 @@ def test_Threshold():
 def test_Segment():
     images = torch.zeros(3, 1, 10, 10)
 
-    # Squares in the middle of the image
-    # vertically at either side of the image.
+    # Squares in the middle of the image vertically at either side of the
+    # image.
     images[0, 0, 4:7, 0:3] = 1
     images[0, 0, 4:7, 7:10] = 1
 
-    # Bands on the top and bottom of the image
-    # that go all the way across.
+    # Bands on the top and bottom of the image that go all the way across.
     images[1, 0, 0:3, 0:10] = 1
     images[1, 0, 7:10, 0:10] = 1
 
-    # Check that we count adjacent diagonally as connected.
-    # This image is one block in the middle vertically that
-    # diagonally touches another block that is above the middle.
-    images[1, 0, 4:7, 4:7] = 1
-    images[1, 0, 0:4, 7:10] = 1
+    # One block that is diagonally adjacent to another. This first block sits
+    # along the middle band of the image.
+    images[2, 0, 4:7, 4:7] = 1
+    images[2, 0, 1:4, 7:10] = 1
+
+    segmenter = SkyrogueSegmenter()
+    segments = segmenter.Segment(images)
+
+    assert len(segments) == 3
+
+    assert len(segments[0]) == 2
+    assert segments[0][0].size() == (3, 3)
+    assert segments[0][1].size() == (3, 3)
+
+    assert len(segments[1]) == 0
+
+    assert len(segments[2]) == 1
+    assert segments[2][0].size() == (6, 6)
